d86c405989e17fbee814699db8685d84c0e636d1
George Ramirez <george.ramirez2@gmail.com>
2016-12-12 11:48:09 -0600

    working  need to style

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,68 @@
+$.getJSON('/articles', function(data) {
+  for (var i = 0; i<data.length; i++){
+    $('#articles').append('<img src="http://www.ethertongallery.com/' + data[i].link + '">' + '<p data-id="' + data[i]._id + '">'+ data[i].title + '</p><br />');
+  }
+});
+
+
+
+$(document).on('click', 'p', function(){
+  $('#notes').empty();
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#notes').append('<h2>' + data.title + '</h2>');
+      $('#notes').append('<input id="titleinput" name="title" >');
+      $('#notes').append('<textarea id="bodyinput" name="body"></textarea>');
+      $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+
+      if(data.note){
+        $('#titleinput').val(data.note.title);
+        $('#bodyinput').val(data.note.body);
+      }
+    });
+});
+
+// Click delete note
+
+$(document).on('click', '#deletenote', function(){
+  var thisId = $(this).attr('data-id');
+  console.log(thisId);
+
+  $.ajax({
+    method: "POST",
+    url: "/deletenote/" + thisId
+  })
+    .done(function() {
+ 
+      $('#titleinput').val("");
+      $('#bodyinput').val("");
+    });
+});
+
+$(document).on('click', '#savenote', function(){
+  var thisId = $(this).attr('data-id');
+  console.log(thisId);
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $('#titleinput').val(),
+      body: $('#bodyinput').val()
+    }
+  })
+    .done(function( data ) {
+      console.log(data);
+    //   $('#notes').empty();
+    });
+      $('#titleinput').val("");
+      $('#bodyinput').val("");
+    // });
+
+});
